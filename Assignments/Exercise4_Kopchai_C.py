3bc0dbf83373a96ca3d92f340f5efa4dee728b31
kop147 <kop147@gmail.com>
2021-10-27 17:59:39 +0700

    Create Exercise4_Kopchai_C.py

---
+++ Assignments/Exercise4_Kopchai_C.py
@@ -0,0 +1,9 @@
+FB = 78.5
+GB = 84.6
+ICS = 49.8
+CP = 64.8
+print( "---------Your Score-----------")
+print "Foundation English       :" ,FB
+print "General Business         :" ,GB
+print "Introduction to Computer :" ,ICS
+print "Computer Programming     :" ,CP
