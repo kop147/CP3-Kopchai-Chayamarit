4249ffd38ea7c469b736a2566fdb983aa6df99be
kop147 <kop147@gmail.com>
2021-10-28 14:33:22 +0700

    Create Lecture31(input).py

---
+++ Lectures/Lecture31(input).py
@@ -0,0 +1,14 @@
+a = "10"
+b = 5
+c = int(a) + b
+d = a+str(b)
+e = float(a)+b
+f = bool(a)+b
+a1 = 1<0
+a2 = 1>0
+print c
+print d
+print e
+print f
+print bool(a1),bool(a2)
+print bool(a1)+0, bool(a2)+0
