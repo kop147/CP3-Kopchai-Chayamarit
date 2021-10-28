575341cd4b783a3df71c5a1b39907dfa7a1f97bb
kop147 <kop147@gmail.com>
2021-10-28 14:59:54 +0700

    Create Exercise5_2_Kopchai_C.py

---
+++ Assignments/Exercise5_2_Kopchai_C.py
@@ -0,0 +1,4 @@
+distance = float(input("How far is it in Km ( from 1 ahead)?"))
+time     = float(input("how long is going to take in second( from 1 ahead)?"))
+velocity = distance/(time/3600)
+print(velocity,"km/h")
