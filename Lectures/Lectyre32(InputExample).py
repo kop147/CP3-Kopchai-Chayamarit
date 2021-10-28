97d4d88c11f500a455d25e13451192c788d6e789
kop147 <kop147@gmail.com>
2021-10-28 14:33:35 +0700

    Create Lectyre32(InputExample).py

---
+++ Lectures/Lectyre32(InputExample).py
@@ -0,0 +1,19 @@
+#ex1
+'''x = input() # from keyboard
+print(type(x))
+print ("hello",x)
+print(type(x))'''
+#ex2
+'''name = input("what is your first name?")
+lastName= input("what is your surname?")
+print (name,lastName)'''
+
+# Home Work
+a = input("please input one number")
+b = input("please in put another number")
+answer = int(a)+int(b)
+
+print (answer)
+
+
+
