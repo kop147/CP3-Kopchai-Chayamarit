231763265aff3e7b85e735a8c1009b3527fe4475
kop147 <kop147@gmail.com>
2021-10-28 23:11:36 +0700

    routine upload

---
+++ Lectures/Tip-how to print in the same row on Loop.py
@@ -0,0 +1,23 @@
+x = 0
+while x < 5:
+    x += 1
+    print(x, end = "")
+print(end="\n")
+x = 0
+while x < 5:
+    x += 1
+    print(x,end = "-")
+print(end="\n")
+x = 0
+while x < 5:
+    x += 1
+    if x < 5 :
+        print(x, end="$")
+    else: print(x)
+x = 0
+while x < 5:
+    x += 1
+    print(x, sep = "t", end="")
+
+
+
