297978388b6108022563d679960aba704c8425f7
kop147 <kop147@gmail.com>
2021-10-28 14:33:14 +0700

    Create Lecture28(Booleanoperation).py

---
+++ Lectures/Lecture28(Booleanoperation).py
@@ -0,0 +1,16 @@
+'''x = True
+y = False
+z1 = x and y
+z2 = x or y
+a = 10
+b = 5
+c1 = a > b
+
+print z1
+print z2
+print c1 '''
+# Example2
+
+age = 40.3
+print ("Hello  Are your age qualify to this place ?")
+print (age > 18)
