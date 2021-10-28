d92f117ce4487a4637a26cff919c5123be518fbe
kop147 <kop147@gmail.com>
2021-10-28 14:33:10 +0700

    Create Lecture26(mathoperation).py

---
+++ Lectures/Lecture26(mathoperation).py
@@ -0,0 +1,15 @@
+# x = 10  # type: int
+# y = 5
+#z = x+y
+#print z
+
+#example2
+money = 150
+incomePerDay = 300
+costPerDay = 120
+result = money+(incomePerDay*30)-(costPerDay*30)
+print(result)
+
+# operation sequence
+test1 = money+incomePerDay*30/30-costPerDay*30
+print test1
