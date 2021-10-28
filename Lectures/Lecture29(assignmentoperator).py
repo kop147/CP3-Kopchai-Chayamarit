9735c84182380d5a5cc9324814f34f3d2f5ee839
kop147 <kop147@gmail.com>
2021-10-28 14:33:19 +0700

    Create Lecture29(assignmentoperator).py

---
+++ Lectures/Lecture29(assignmentoperator).py
@@ -0,0 +1,23 @@
+# Ex1
+'''a = 10
+b = 5
+c = a + b
+b = 10
+print b'''
+
+#Ex2
+
+'''a = 10
+b = 12
+c = a + b
+a += 5 # a = a + 5
+# += mean ADD
+b -= 2
+print a,b'''
+
+# ex3
+
+money = 1000
+money -= 50
+money -= 20
+print(money)
