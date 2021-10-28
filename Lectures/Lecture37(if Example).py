b0fae370b95b498b30d6442d89da6bb6735cb9e5
kop147 <kop147@gmail.com>
2021-10-28 19:06:11 +0700

    Create Lecture37(if Example).py

---
+++ Lectures/Lecture37(if Example).py
@@ -0,0 +1,9 @@
+user_input = input("username")
+pass_input = str(input("password"))
+user = "kop"
+password = str(123456)
+
+if (user_input == user) and (pass_input == password):
+    print("Login successful")
+else:
+    print("Please Login again!!!!")
